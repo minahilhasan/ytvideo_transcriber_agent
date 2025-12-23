ad7d0fdf32a785f19d1c27227e87e7936d98bcde
minahilhasan <minahilhasan@gmail.com>
2025-12-23 13:44:53 +0500

    Update

--- app.py
+++ app.py
@@ -4,7 +4,7 @@ from agent import aiagent
 
 def main():
     st.title("YouTube Videos Transcriber")
-
+    st.write("First enter the title of the video you want to transcribe, then click run agent. The agent will first show you the youtube link that it is transcribing and will then move on to display the full transcription")
     user_request = st.text_input(
         "Enter the topic you would like to search for:"
     )
